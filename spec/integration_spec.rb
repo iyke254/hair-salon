fac2e034a826b42903a97ed65a1e80bb541e06a8
iyke <nderiony95@gmail.com>
2017-07-14 12:26:53 +0300

    2nd spec on integ

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -13,3 +13,14 @@ describe('add new stylist', {:type => :feature}) do
     expect(page).to have_content('Information for Wonderwoman')
   end
 end
+
+describe('change stylist name', {:type => :feature}) do
+  it('allows user to change stylist name') do
+    stylist = Stylist.new({:name => 'Wonderwoman', :id => nil})
+    stylist.save()
+    visit("/stylist/#{stylist.id()}")
+    fill_in("new_name", :with => 'Batman')
+    click_button('Update Name')
+    expect(page).to have_content('Batman')
+  end
+end
