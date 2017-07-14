6815782ccf36b822b9641021bcfd319e62d24f38
iyke <nderiony95@gmail.com>
2017-07-14 12:05:45 +0300

    added the 2nd spec to the client spec

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,9 +4,15 @@ describe(Client) do
 
 	describe('#initialize') do 
 		it('returns the name of the client') do
-			test_client = Client.new({:name => 'Harry Potter', :id => nil, :stylist_id => 1})
-			expect(test_client.name()).to(eq('Harry Potter'))
+			test_client = Client.new({:name => 'Eragon', :id => nil, :stylist_id => 1})
+			expect(test_client.name()).to(eq('Eragon'))
 		end
 	end
 
-	
+	describe('#save') do
+   	 	it('saves client with a name') do
+     		test_client = Client.new({:name => 'Eragon', :id => nil, :stylist_id => 1})
+      		test_client.save()
+      		expect(Client.all()).to(eq([test_client]))
+    	end
+    end
