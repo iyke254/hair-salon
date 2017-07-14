01b4b03b6d839e11a515b97cdc6416bc774b147f
iyke <nderiony95@gmail.com>
2017-07-14 12:45:01 +0300

    all tests added

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,24 +3,45 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('add new stylist', {:type => :feature}) do
+describe('add new stylist', type: :feature}) do
   it('allows user to add a stylist and view page') do
     visit('/')
     fill_in('stylist_name', :with => 'Wonderwoman')
     click_button('Add Stylist')
     click_link('Return to Homepage')
-    click_link('Wonderwoman')
     expect(page).to have_content('Information for Wonderwoman')
   end
 end
 
-describe('change stylist name', {:type => :feature}) do
-  it('allows user to change stylist name') do
-    stylist = Stylist.new({:name => 'Wonderwoman', :id => nil})
-    stylist.save()
-    visit("/stylist/#{stylist.id()}")
-    fill_in("new_name", :with => 'Batman')
-    click_button('Update Name')
-    expect(page).to have_content('Batman')
-  end
+describe('viewing all stylists', type: :feature) do
+	it('allows users to see all stylists') do
+		stylist = Stylist.new(name => 'Wonderwoman', id: nil)
+		stylist.save
+		visit('/')
+		click_link('See all Stylists')
+		expect(page).to have_content(stylist.name)
+	end	
 end
+
+describe('seeing details for a single Stylist', type: :feature) do
+	it('allows a user to click a stylist and see her clients and their details') do
+		test_stylist = Stylist.new(name: 'Hvedra', id: nil)
+		test_stylist.save
+		test_client = Client.new(description: 'Learn SQL', stylist_id: test_stylist.id)
+		test_client.save
+		visit('/')
+		click_link(test_stylist.name)
+		expect(page).to have_content(test_client.description)
+	end	
+end
+
+describe('adding clients to a stylist', type: :feature) do
+	it('allows a user to add a client to a stylist') do
+		test_stylist = Stylist.new(name: 'Islanzadi')
+		test_stylist.save
+		visit("stylist/#{test_stylist.id}")
+		fill_in('Description', with: 'Learn SQL')
+		click_button('Add Stylist')
+		expect(page).to have_content('Success')
+	end
+end		
