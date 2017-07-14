4414eabc798b9d90a62eb3270db5348e56ec1d6f
iyke <nderiony95@gmail.com>
2017-07-14 12:25:51 +0300

    first spec on integration

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,3 +3,13 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
+describe('add new stylist', {:type => :feature}) do
+  it('allows user to add a stylist and view page') do
+    visit('/')
+    fill_in('stylist_name', :with => 'Wonderwoman')
+    click_button('Add Stylist')
+    click_link('Return to Homepage')
+    click_link('Wonderwoman')
+    expect(page).to have_content('Information for Wonderwoman')
+  end
+end
