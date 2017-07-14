af4e1addfe63d9d07799021722da907752735f3d
iyke <nderiony95@gmail.com>
2017-07-14 13:34:43 +0300

    new tests

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,67 +1,39 @@
 require('spec_helper')
 
-describe(Client) do 
-
-	describe('#initialize') do 
-		it('returns the name of the client') do
-			test_client = Client.new({:name => 'Eragon', :id => nil, :stylist_id => 1})
-			expect(test_client.name()).to(eq('Eragon'))
-		end
-	end
-
-	describe('#save') do
-   	 	it('saves client with a name') do
-     		test_client = Client.new({:name => 'Eragon', :id => nil, :stylist_id => 1})
-      		test_client.save()
-      		expect(Client.all()).to(eq([test_client]))
-    	end
+describe(Client) do
+  describe('.all') do
+    it('is empty at first') do
+      expect(Client.all).to(eq([]))
     end
-
-    describe('#==') do
-    	it('is the same client if the names are the same text') do
-      		test_client = Client.new({:name => 'Eragon', :id => nil, :stylist_id => 1})
-      		new_client = Client.new({:name => 'Eragon', :id => nil, :stylist_id => 1})
-      		expect(test_client).to(eq(new_client))
-    	end
-    end
-
-    describe('#delete') do
-    	it('lets you delete a client in the database') do
-      	    test_client = Client.new({:name => 'Eragon', :id => nil, :stylist_id => 1})
-      		test_client.save()
-      		test_client2 = Client.new({:name => 'Roran', :id => nil, :stylist_id => 1})
-      		test_client2.save()
-      		test_client.delete()
-      		expect(Client.all()).to(eq([test_client2]))
-    	end
-    end
-
-    describe('.find') do
-    	it('lets you find the client by id') do
-      		test_client = Client.new({:name => 'Eragon', :id => nil, :stylist_id => 1})
-      		test_client.save()
-      		expect(Client.find(test_client.id())).to(eq(test_client))
-    	end
-  	end
-
-  	describe('#update') do
-    	it('lets you update clients by name in the database') do
-     		test_client = Client.new({:name => 'Eragon', :id => nil, :stylist_id => 1})
-       		test_client.save()
-       		test_client.update({:name => 'Galbatorix'})
-       		expect(test_client.name()).to(eq('Galbatorix'))
-    	end
-   	end
-
-   	describe('.get_client_group') do
-       	it('returns all the clients of a stylist') do
-      		new_client = Client.new(:name => 'Brom', :id => nil, :stylist_id => 2)
-      		new_client.save()
-      		new_client1 = Client.new(:name => 'Nasuada', :id => nil, :stylist_id => 2)
-     		new_client1.save()
-      		new_client2 = Client.new(:name => 'Hrothgar', :id => nil, :stylist_id => 3)
-      		new_client2.save()
-      		expect(Client.get_client_group(2)).to(eq([new_client, new_client1]))
-    	end
-  	end
   end
+
+  describe('#save') do
+    it('adds a client to the array of saved stylists') do
+      test_client = Client.new(description: 'learn SQL', stylist_id: 1)
+      test_client.save
+      expect(Client.all).to(eq([test_client]))
+    end
+  end
+
+  describe('#description') do
+    it('lets you read the description out') do
+      test_client = Client.new(description: 'learn SQL', stylist_id: 1)
+      expect(test_client.description).to(eq('learn SQL'))
+    end
+  end
+
+  describe('#stylist_id') do
+    it('lets you read the stylist ID out') do
+      test_client = Client.new(description: 'learn SQL', stylist_id: 1)
+      expect(test_client.stylist_id).to(eq(1))
+    end
+  end
+
+  describe('#==') do
+    it('is the same client if it has the same description and stylist ID') do
+      client1 = Client.new(description: 'learn SQL', stylist_id: 1)
+      client2 = Client.new(description: 'learn SQL', stylist_id: 1)
+      expect(client1).to(eq(client2))
+    end
+  end
+end
