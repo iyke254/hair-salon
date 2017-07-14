8e619f301692aea8b97933b93793ebfd4241ed4e
iyke <nderiony95@gmail.com>
2017-07-14 12:51:27 +0300

    all tests added

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -2,12 +2,18 @@ require('spec_helper')
 
 describe(Stylist) do 
 
-	describe('#initialize') do
-		it('returns the name of the stylist') do
-			test_stylist = Stylist.new({:name => 'WonderWoman', :id => nil})
-			expect(test_stylist.name()).to(eq('WonderWoman'))
-		end	
-	end	
+	describe('.all') do
+    	it('starts off with no stylists') do
+      		expect(Stylist.all).to(eq([]))
+    	end
+  	end
+
+  	describe('#name') do
+    	it('tells you its name') do
+      		stylist = Stylist.new(name: 'WonderWoman', id: nil)
+      		expect(stylist.name).to(eq('WonderWoman'))
+    	end
+  	end
 
 	describe('#save') do
 		it('saves stylist with name') do
