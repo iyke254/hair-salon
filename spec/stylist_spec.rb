539dd54df167a28d9e2afc2532e0d046d4106a3b
iyke <nderiony95@gmail.com>
2017-07-14 13:29:09 +0300

    new tests

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -61,25 +61,34 @@ describe(Stylist) do
       expect(test_stylist.clients).to(eq([test_client, test_client2]))
     end
   end
+   describe('#update') do
+    it('lets you update stylists in the database') do
+      stylist = Stylist.new(name: 'WonderWoman', id: nil)
+      stylist.save
+      stylist.update(name: 'Thor')
+      expect(stylist.name).to(eq('Thor'))
+    end
+  end
 
-	describe('#update') do
-		it('lets you update stylists by name in the database')	do
-			test_stylist = Stylist.new({:name => "Hermoine", :id => nil})
-			test_stylist.save()	
-			test_stylist.update({:name => "Emma"})
-			expect(test_stylist.name()).to(eq("Emma"))
-		end
-	end
-
-	describe('#delete') do
-		it('lets you delete a stylist from the database') do
-			test_stylist = Stylist.new({:name => "Black Widow", :id => nil})
-			test_stylist.save()
-			test_stylist2 = Stylist.new({:name => 'Joan of Arc', :id =>nil})
-			test_stylist2.save()
-			test_stylist.delete()
-			expect(Stylist.all()).to(eq([test_stylist2]))
-		end
-	end
+  describe('#delete') do
+    it('lets you delete a stylist from the database') do
+      stylist = Stylist.new(name: 'WonderWoman', id: nil)
+      stylist.save
+      stylist2 = Stylist.new(name: 'Thor', id: nil)
+      stylist2.save
+      stylist.delete
+      expect(Stylist.all).to(eq([stylist2]))
+    end
+    it("deletes a stylist's clients from the database") do
+      stylist = Stylist.new(name: 'WonderWoman', id: nil)
+      stylist.save
+      client = Client.new(description: 'learn SQL', stylist_id: stylist.id)
+      client.save
+      client2 = Client.new(description: 'Review Ruby', stylist_id: stylist.id)
+      client2.save
+      stylist.delete
+      expect(Client.all).to(eq([]))
+    end
+  end
 end			
 
