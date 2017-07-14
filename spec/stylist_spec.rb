385ed350f3589cfaab54e9b76dc6bcd700b7d7a6
iyke <nderiony95@gmail.com>
2017-07-14 11:34:12 +0300

    added first test in stylist spec

---
+++ spec/stylist_spec.rb
@@ -0,0 +1,12 @@
+require('spec_helper')
+
+describe(Stylist) do 
+
+	describe('#initialize') do
+		it('returns the name of the stylist') do
+			test_stylist = Stylist.new({:name => 'WonderWoman', :id => nil})
+			expect(test_stylist.name()).to(eq('WonderWoman'))
+		end	
+	end	
+
+	
